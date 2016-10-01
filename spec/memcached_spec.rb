d2b3ddbfe4c2694fb71bc956e439235441bf5f44
Sam Phippen <samphippen@googlemail.com>
2016-10-01 16:38:28 +0100

    Add a test for multiple connections.

--- spec/memcached_spec.rb
+++ spec/memcached_spec.rb
@@ -3,6 +3,7 @@ require "memcached"
 
 RSpec.describe "decachedmem" do
   let(:cache) { Memcached.new("localhost:11211") }
+  let(:cache2) { Memcached.new("localhost:11211") }
   it "doesn't choke when setting a key" do
     expect {
       cache.set("foo", "37")
@@ -19,5 +20,14 @@ RSpec.describe "decachedmem" do
   it "can get a value it has set" do
     cache.set("foo", "37")
     expect(cache.get("foo")).to eq("37")
+  end
+
+  it "supports multiple connections" do
+    cache.set("foo1", "37")
+    cache2.set("foo2", "12")
+    aggregate_failures do
+      expect(cache.get("foo2")).to eq("12")
+      expect(cache2.get("foo1")).to eq("37")
+    end
   end
 end
